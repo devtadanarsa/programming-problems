76b5d62dd42708b834b777cfa31f4e2200a40167
Devtaaa <devtadanarsa69@gmail.com>
2024-05-01 22:29:29 +0800

    top_k_frequent_elements

---
+++ arrays-hashing/top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+def topKFrequent(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: List[int]
+    """
+    hashTable = {}
+    for num in nums:
+        if num in hashTable:
+            hashTable[num].append(num)
+        else:
+            hashTable[num] = [num]
+            
+    tempList = list(hashTable.values())
+    tempList.sort(key=len, reverse=True)
+    
+    result = []
+    for i in range(k):
+        result.append(tempList[i][0])
+    
+    return result
